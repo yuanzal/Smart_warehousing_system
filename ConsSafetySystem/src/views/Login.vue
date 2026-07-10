9cfd0debb445618a3c72af6af77a5e8c84a25c9a
Your Name <email@example.c>
2026-07-10 11:04:51 +0800

    Initial commit

---
+++ ConsSafetySystem/src/views/Login.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="login-container">
+    <div class="login-box">
+      <div class="login-header">
+        <h2>建筑安全智能监控平台</h2>
+        <p>Intelligent Building Safety Monitoring Platform</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+
